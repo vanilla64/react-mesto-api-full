a61797fab6f6179b132f3eb701b8a079f6bb41ba
Sergei Mashin <vanilla@MacBook-Pro-Sergej.local>
2020-11-25 22:48:54 +0300

    dont die please

--- frontend/src/utils/UserAuth.js
+++ frontend/src/utils/UserAuth.js
@@ -60,6 +60,8 @@ class UserAuth {
 
 }
 
-const auth = new UserAuth('http://localhost:4000')
+// const auth = new UserAuth('http://localhost:4000')
+// const auth = new UserAuth('http://vanillaen.students.nomoreparties.xyz:4000')
+const auth = new UserAuth('http://84.201.172.179:4000')
 
 export default auth;
